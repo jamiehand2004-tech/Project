cb5f6ea0fdcb186f32dfe53bd5b06646fcef2ed8
James Hand <c23428162@mytudublin.ie>
2025-11-05 12:06:01 +0000

    Add initial project files: .gitignore and main.cpp

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main() {
+    // Your code here
+    int x;
+    cin >> x; // Read an integer from input
+    cout << "You entered: " << x << endl; // Output the integer
+    cin.get(); // Wait for user input before closing
+    return 0;
+}
