f34d3345f1649be77a6a4d2082cc0f4c5b4f2240
James Hand <c23428162@mytudublin.ie>
2025-11-12 11:22:15 +0000

    added project desirables and removed quiz.cpp.

---
+++ list.cpp
@@ -0,0 +1,70 @@
+/*Final deliverables for project: (anything with a ^ needs to be done.)
+
+4. Multi-User Quiz Application
+
+A console application that allows users to take C++ quizzes and receive scores.
+
+- User registration and login
+^   >Mostly done, needs testing and refinement, and an implementation of save-state to file.
+^   >Login system needs consistency and undesired errors fixed.
+
+- Create quizzes with multiple-choice questions
+^   >Need to generate sets of questions and answers, from file.
+^   >figure out how to store them in text file.
+
+- Allow users to take quizzes and see their scores
+    >Implemented basic quiz-taking functionality. Needs polishing and better user interface.
+
+- Leaderboard to display top scores
+    >Implemented a simple leaderboard. Needs sorting and better display format.
+^   >Needs to be saved to a file/allow for multiple users.
+
+- Save quiz data and user scores to a file
+^   >need to figure out the save and loading across multiple instances.
+
+Roles:
+
+- Quiz Logic: Implements the quiz-taking and scoring mechanics.
+
+
+- User Management: Manages user accounts and score tracking.
+
+
+- User Interface: Designs the quiz navigation and displays results.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+*/
